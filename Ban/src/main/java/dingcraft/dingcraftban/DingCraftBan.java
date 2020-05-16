6645f4ddc8d9d4e816172b0b746fbc27033d5505
haohaostudio <919835909@qq.com>
2020-05-16 15:49:02 +0800

    1

---
+++ Ban/src/main/java/dingcraft/dingcraftban/DingCraftBan.java
@@ -0,0 +1,17 @@
+package dingcraft.dingcraftban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DingCraftBan extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
